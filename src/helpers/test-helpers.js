1d4e53857fab379eb718cea2b17552e40cc132a3
Genyus <genyus@gmail.com>
2025-05-07 08:39:36 -0400

    style: formats code to pass ESLint rules

--- src/helpers/test-helpers.js
+++ src/helpers/test-helpers.js
@@ -1,28 +1,28 @@
-const getMaxScoreForTest = (runnerResult) => runnerResult.max_score || 0;
+const getMaxScoreForTest = (runnerResult) => runnerResult.max_score || 0
 
 const getTotalMaxScore = (runnerResults) => {
-  return runnerResults.reduce((acc, { results }) => acc + results.max_score, 0);
-};
+  return runnerResults.reduce((acc, {results}) => acc + results.max_score, 0)
+}
 
-const totalPercentageReducer = (acc, { score, weight, maxScore }) => {
-  return acc + ((score || 0) / (maxScore || 1)) * weight;
-};
+const totalPercentageReducer = (acc, {score, weight, maxScore}) => {
+  return acc + ((score || 0) / (maxScore || 1)) * weight
+}
 
 const getTestScore = (runnerResult) => {
-  return runnerResult.tests.reduce((acc, { score }) => acc + (score || 0), 0);
-};
+  return runnerResult.tests.reduce((acc, {score}) => acc + (score || 0), 0)
+}
 
 const getTestWeight = (maxScore, allMaxScores) => {
   if (maxScore === 0) {
-    return (0).toFixed(1);
+    return (0).toFixed(1)
   }
-  const weight = allMaxScores !== 0 ? (maxScore / allMaxScores) * 100 : 0;
+  const weight = allMaxScores !== 0 ? (maxScore / allMaxScores) * 100 : 0
 
-  return Math.round(weight).toFixed(2);
-};
+  return Math.round(weight).toFixed(2)
+}
 
-exports.getMaxScoreForTest = getMaxScoreForTest;
-exports.getTotalMaxScore = getTotalMaxScore;
-exports.totalPercentageReducer = totalPercentageReducer;
-exports.getTestScore = getTestScore;
-exports.getTestWeight = getTestWeight;
+exports.getMaxScoreForTest = getMaxScoreForTest
+exports.getTotalMaxScore = getTotalMaxScore
+exports.totalPercentageReducer = totalPercentageReducer
+exports.getTestScore = getTestScore
+exports.getTestWeight = getTestWeight
