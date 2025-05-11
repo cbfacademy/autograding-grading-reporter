258715f82b0dd7bfdd166f4976cca7f8a7c2f585
Genyus <genyus@gmail.com>
2025-05-11 15:26:36 -0400

    format: fix ESLint/Prettier warnings

--- src/helpers/test-helpers.js
+++ src/helpers/test-helpers.js
@@ -1,4 +1,4 @@
-const core = require("@actions/core");
+const core = require('@actions/core')
 
 const getMaxScoreForTest = (runnerResult) => runnerResult.max_score || 0
 
--- src/notify-classroom.js
+++ src/notify-classroom.js
@@ -1,32 +1,32 @@
-const core = require("@actions/core");
+const core = require('@actions/core')
 
 exports.NotifyClassroom = async function NotifyClassroom(runnerResults) {
   // combine max score and total score from each {runner, results} pair
   // if max_score is greater than 0 run the rest of this code
-  const { totalPoints, maxPoints } = runnerResults.reduce(
-    (acc, { results }) => {
-      if (!results.max_score) return acc;
+  const {totalPoints, maxPoints} = runnerResults.reduce(
+    (acc, {results}) => {
+      if (!results.max_score) return acc
 
-      acc.maxPoints += results.max_score;
-      results.tests.forEach(({ score }) => {
-        acc.totalPoints += score;
-      });
+      acc.maxPoints += results.max_score
+      results.tests.forEach(({score}) => {
+        acc.totalPoints += score
+      })
 
-      return acc;
+      return acc
     },
-    { totalPoints: 0, maxPoints: 0 }
-  );
-  if (!maxPoints) return;
+    {totalPoints: 0, maxPoints: 0},
+  )
+  if (!maxPoints) return
 
-  const text = `Points ${totalPoints}/${maxPoints}`;
-  const summary = JSON.stringify({ totalPoints, maxPoints })
+  const text = `Points ${totalPoints}/${maxPoints}`
+  const summary = JSON.stringify({totalPoints, maxPoints})
 
   // create notice annotations with the final result and summary
   core.notice(text, {
-    title: "Autograding complete",
+    title: 'Autograding complete',
   })
 
   core.notice(summary, {
-    title: "Autograding report",
+    title: 'Autograding report',
   })
-};
+}
