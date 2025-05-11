fcbb7a9bb26d243ee9f0d47f8137111d95b089f1
Genyus <genyus@gmail.com>
2025-05-11 15:13:45 -0400

    format: fixes ESLint/Prettier warnings

--- src/main.js
+++ src/main.js
@@ -1,34 +1,33 @@
-const core = require("@actions/core");
-const { ConsoleResults } = require("./console-results");
-const { NotifyClassroom } = require("./notify-classroom");
+const core = require('@actions/core')
+const {ConsoleResults} = require('./console-results')
+const {NotifyClassroom} = require('./notify-classroom')
 
 try {
   const runnerResults = core
-    .getInput("runners")
-    .split(",")
+    .getInput('runners')
+    .split(',')
     .map((runner) => {
-      const encodedResults = process.env[`${runner.trim().toUpperCase()}_RESULTS`];
-      const json = Buffer.from(encodedResults, "base64").toString("utf-8");
-      return { runner, results: JSON.parse(json) };
-    });
+      const encodedResults = process.env[`${runner.trim().toUpperCase()}_RESULTS`]
+      const json = Buffer.from(encodedResults, 'base64').toString('utf-8')
+      return {runner, results: JSON.parse(json)}
+    })
 
+  ConsoleResults(runnerResults)
+  NotifyClassroom(runnerResults)
 
-  ConsoleResults(runnerResults);
-  NotifyClassroom(runnerResults);
-
-  if (runnerResults.some((r) => r.results.status === "fail")) {
-    core.setFailed("Some tests failed.");
+  if (runnerResults.some((r) => r.results.status === 'fail')) {
+    core.setFailed('Some tests failed.')
   } else if (runnerResults.some((r) => r.results.status === 'error')) {
-    core.setFailed("Some tests errored.");
+    core.setFailed('Some tests errored.')
   }
 } catch (error) {
-  const input = core.getInput("runners");
+  const input = core.getInput('runners')
   const pattern = /^([a-zA-Z0-9]+,)*[a-zA-Z0-9]+$/
   if (!pattern.test(input)) {
-    console.error("The runners input must be a comma-separated list of strings.");
-    core.setFailed("The runners input must be a comma-separated list of strings.");
+    console.error('The runners input must be a comma-separated list of strings.')
+    core.setFailed('The runners input must be a comma-separated list of strings.')
   } else {
     console.error(error.message)
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
